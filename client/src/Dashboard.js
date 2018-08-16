0669baabf2c838bf57e0a4eeb595b2da52650c41
arkn98 <arun19091998@gmail.com>
2018-08-17 00:22:49 +0530

    css changes

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -3,13 +3,24 @@ import styles from "./Dashboard.css";
 import Sidenav from "./components/Sidenav";
 
 class Dashboard extends Component {
+  state = {
+    notifCount: 0,
+    mouseX: 0,
+    mouseY: 0
+  };
+
   render() {
     return (
       <div className={styles.dashMount}>
         <div className={styles.sideNavWrapper}>
-          <Sidenav />
+          <Sidenav notifCount={this.state.notifCount} />
         </div>
-        <div className={styles.mainWrapper}>abcd</div>
+        <div className={styles.mainWrapper}>
+          <a href="/">123454321</a>
+        </div>
+        <div>
+          <a href="/">123</a>
+        </div>
       </div>
     );
   }
--- client/src/components/Sidenav.js
+++ client/src/components/Sidenav.js
@@ -1,88 +1,152 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./Sidenav.css";
 import { Link, NavLink } from "react-router-dom";
 
-const Sidenav = props => {
-  return (
-    <div style={{ height: "100%" }}>
-      <div className={styles.sideNav}>
-        <div className={styles.menu}>
-          <div className={styles.logo}>
-            <Link to="/dashboard">LMS</Link>
-            <div className={styles.customHeaderIcons}>
-              <a
-                href="https://github.com/arkn98/lms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i
-                  className={`icon ion-logo-github ${styles.customHeaderIcon}`}
-                />
-              </a>
-              <Link to="/">
-                <i
-                  className={`icon ion-md-information-circle ${
-                    styles.customHeaderIcon
-                  }`}
-                />
+class Sidenav extends Component {
+  state = {
+    isSettingsMenuVisible: false,
+    mouseX: 0,
+    mouseY: 0
+  };
+
+  settingsMenuClickHandler = event => {
+    this.setState({ isSettingsMenuVisible: !this.state.isSettingsMenuVisible });
+  };
+
+  render() {
+    let settingsMenuStyles = [];
+    let settingsIconSelector = [];
+    settingsMenuStyles.push(styles.settingsMenu);
+    settingsIconSelector.push(styles.iconSelector);
+    if (this.state.isSettingsMenuVisible) {
+      settingsIconSelector.push(styles.iconSelectorHovered);
+      settingsMenuStyles.push(styles.settingsMenuVisible);
+    }
+    return (
+      <div style={{ height: "100%" }}>
+        <div className={styles.sideNav}>
+          <div className={styles.menu}>
+            <div className={styles.logo}>
+              <Link href="/" to="/dashboard">
+                LMS
               </Link>
-            </div>
-          </div>
-          <div className={styles.scrollMenuWrapper}>
-            <div className={styles.scroller}>
-              <span>
-                <div className={styles.menuItem}>
-                  <Link to="/">
-                    <i className={`icon ion-md-easel ${styles.customIcon}`} />
-                    &nbsp;
-                    <div
-                      style={{ display: "inline" }}
-                      className={styles.menuText}
-                    >
-                      Dashboard
-                    </div>
-                  </Link>
+              <div className={styles.menuText}>v2.0.1</div>
+              <div className={styles.customHeaderIcons}>
+                <div className={styles.iconWrapper}>
+                  <a
+                    href="https://github.com/arkn98/lms"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i
+                      className={`icon ion-logo-github ${
+                        styles.customHeaderIcon
+                      }`}
+                    />
+                  </a>
                 </div>
-                <header className={styles.menuHeader}>Leaves</header>
-                <div className={styles.menuItem}>
+                <div className={styles.iconWrapper}>
                   <Link to="/">
                     <i
-                      className={`icon ion-md-list-box ${styles.customIcon}`}
+                      className={`icon ion-md-information-circle ${
+                        styles.customHeaderIcon
+                      }`}
                     />
-                    &nbsp;
-                    <div
-                      style={{ display: "inline" }}
-                      className={styles.menuText}
-                    >
-                      Apply for Leave
-                    </div>
                   </Link>
                 </div>
-                <header className={styles.menuHeader}>Reports</header>
-                <div className={styles.menuItem}>
-                  <Link to="/">
-                    <i className={`icon ion-md-stats ${styles.customIcon}`} />
-                    &nbsp;
-                    <div
-                      style={{ display: "inline" }}
-                      className={styles.menuText}
-                    >
-                      View Reports
-                    </div>
-                  </Link>
-                </div>
-                {/* <div className={styles.seperator} /> */}
-                <header className={styles.menuHeader}>
-                  Notifications - (Count)
-                </header>
-              </span>
+              </div>
+            </div>
+            <div className={styles.scrollMenuWrapper}>
+              <div className={styles.scroller}>
+                <span>
+                  <div className={styles.menuItem}>
+                    <Link to="/">
+                      <i className={`icon ion-md-easel ${styles.customIcon}`} />
+                      &nbsp;
+                      <div
+                        style={{ display: "inline" }}
+                        className={styles.menuText}
+                      >
+                        Dashboard
+                      </div>
+                    </Link>
+                  </div>
+                  <div className={styles.menuItem}>
+                    <Link to="/">
+                      <i
+                        className={`icon ion-md-list-box ${styles.customIcon}`}
+                      />
+                      &nbsp;
+                      <div
+                        style={{ display: "inline" }}
+                        className={styles.menuText}
+                      >
+                        Apply for Leave
+                      </div>
+                    </Link>
+                  </div>
+                  <div className={styles.menuItem}>
+                    <Link to="/">
+                      <i className={`icon ion-md-eye ${styles.customIcon}`} />
+                      &nbsp;
+                      <div
+                        style={{ display: "inline" }}
+                        className={styles.menuText}
+                      >
+                        View Holidays
+                      </div>
+                    </Link>
+                  </div>
+                  <div className={styles.menuItem}>
+                    <Link to="/">
+                      <i className={`icon ion-md-stats ${styles.customIcon}`} />
+                      &nbsp;
+                      <div
+                        style={{ display: "inline" }}
+                        className={styles.menuText}
+                      >
+                        View Reports
+                      </div>
+                    </Link>
+                  </div>
+                  {/* <div className={styles.seperator} /> */}
+                  <header className={styles.menuHeader}>
+                    Notifications - ({this.props.notifCount})
+                  </header>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.userContainer}>
+            <div className={styles.userAvatar}>
+              <i className={`icon ion-md-contact ${styles.customIcon}`} />
+            </div>
+            <div className={styles.userDetails}>
+              <span className={styles.userName}>ark__n</span>
+              <span className={styles.userClass}>#1761</span>
+            </div>
+            <div className={styles.userSettings}>
+              <div
+                className={settingsIconSelector.join(" ")}
+                onClick={this.settingsMenuClickHandler}
+              >
+                <i
+                  className={`icon ion-md-settings ${styles.customIcon}`}
+                  title="Settings"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className={styles.userContainer}>asdjfhsaf</div>
+        <div className={settingsMenuStyles.join(" ")}>
+          <ul>
+            <li>Change Password</li>
+            <li>Logout</li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Sidenav;
